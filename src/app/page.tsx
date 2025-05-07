a677d7af2601c7ad079965ddd954af1d8c3339b1
Maksym Medvediev <vaselisk1997@gmail.com>
2025-05-07 14:03:53 +0300

    Update website files - 5/7/2025, 2:03:51 PM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import { Contact } from "@src/components/sections/Contact";
- 
-export default function Home() {
-   return (
-   <div data-editor-tag="OzkmGY064o"  className="flex min-h-screen flex-col">
-     <Header data-editor-tag="9KcQxq1Mp7"  />
-     <main data-editor-tag="k8Kdz1TeuC"  className="flex-1">
-      <Pricing data-editor-tag="EgCCMZjSO8" />
-      <Contact data-editor-tag="v4xw6sZSUL" />
-     </main>
-     </div>
-   );
- }
+Index: file
+===================================================================
+--- file
++++ file
+@@ -1,13 +1,12 @@
+ import { Contact } from "@src/components/sections/Contact";
+  
+ export default function Home() {
+    return (
+-   <div data-editor-tag="OzkmGY064o"  className="flex min-h-screen flex-col">
+-     <Header data-editor-tag="9KcQxq1Mp7"  />
+-     <main data-editor-tag="k8Kdz1TeuC"  className="flex-1">
+-      <Pricing data-editor-tag="EgCCMZjSO8" />
+-      <Contact data-editor-tag="v4xw6sZSUL" />
++  <div data-editor-tag="nB0HG4MWOh"  className="flex min-h-screen flex-col">
++    <Header data-editor-tag="d1iAw4QSuN"  />
++    <main data-editor-tag="p0Cf3c5FNO"  className="flex-1">
++     <Pricing data-editor-tag="HdXSOvVbAu"  />
+      </main>
+      </div>
+    );
+  }
