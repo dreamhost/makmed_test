69adb8c894046ce9abedb6f87d29c7ce905b1e12
Maksym Medvediev <vaselisk1997@gmail.com>
2025-05-07 14:02:25 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,10 @@ export default function Home() {
    return (
    <div data-editor-tag="OzkmGY064o"  className="flex min-h-screen flex-col">
      <Header data-editor-tag="9KcQxq1Mp7"  />
-     <main data-editor-tag="F08x6hSva9"  className="flex-1">
-       <Pricing data-editor-tag="lxcepMAyom"/>
-       <Contact data-editor-tag="TSbRNKFCUS"/>
-  <div data-editor-tag="xIaJTRi1Hu"  className="flex min-h-screen flex-col">
-    <main data-editor-tag="k8Kdz1TeuC"  className="flex-1">
+     <main data-editor-tag="k8Kdz1TeuC"  className="flex-1">
       <Pricing data-editor-tag="EgCCMZjSO8" />
       <Contact data-editor-tag="v4xw6sZSUL" />
-       </main>
-     <Footer data-editor-tag="KG994gFKe1"  />
-    <Footer data-editor-tag="8ZLIvA8iKT"  />
+     </main>
      </div>
    );
  }
